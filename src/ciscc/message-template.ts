3508223e81ebdc414bc510132dbf74c2b3130049
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-30 20:22:20 +0800

    feat(src/ciscc/message-template): added

---
+++ src/ciscc/message-template.ts
@@ -0,0 +1,16 @@
+import type { Infer } from "myzod";
+import type { StrapiMessagesResponseEntrySchema } from "../strapi/strapi.interface";
+
+export const ComplainMessage = ({
+  created_at,
+  id,
+  message,
+}: Infer<typeof StrapiMessagesResponseEntrySchema>) =>
+  [
+    "---",
+    message,
+    "---",
+    "",
+    `文章 ID：${id}`,
+    `發表時間：${created_at.toLocaleDateString("zh-TW")}`,
+  ].join("\n");
