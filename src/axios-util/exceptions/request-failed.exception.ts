934c1b9e804bbbdb90cd48302ce6bbc01f34498c
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-30 19:16:11 +0800

    fix(src/axios-util/exceptions/request-failed/exception): allow to stringify an Error object

--- src/axios-util/exceptions/request-failed.exception.ts
+++ src/axios-util/exceptions/request-failed.exception.ts
@@ -4,8 +4,13 @@ export class RequestFailedException extends HttpException {
   requestResponse: unknown;
 
   constructor(response: unknown = "") {
+    const responseString = () => {
+      if (typeof response === "object") return JSON.stringify(response);
+      return `${response}`;
+    };
+
     super(
-      `Failed to request to the remote: ${response}`,
+      `Failed to request to the remote: ${responseString()}`,
       HttpStatus.INTERNAL_SERVER_ERROR,
     );
 
