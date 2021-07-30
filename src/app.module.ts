857c2f8074a4734a4e24a8f88c009cc0a8310454
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-30 20:22:51 +0800

    feat(src/app/service): syncApprovedMessages: done!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
 import { AppController } from "./app.controller";
 import { StrapiModule } from "./strapi/strapi.module";
 import { DiscordBotModule } from "./discord-bot/discord-bot.module";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [ConfigModule.forRoot(), StrapiModule, DiscordBotModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    StrapiModule,
+    DiscordBotModule,
+  ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
+import { StrapiService } from "./strapi/strapi.service";
+import { DiscordBotService } from "./discord-bot/discord-bot.service";
+import { ComplainMessage } from "./ciscc/message-template";
+
+@Injectable()
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(
+    private readonly strapiService: StrapiService,
+    private readonly discordService: DiscordBotService,
+  ) {}
+
+  @Cron("* */1 * * * *")
+  async syncApprovedMessages() {
+    this.logger.log("Start syncing approved messages...");
+
+    const unpublishedMessages =
+      await this.strapiService.getApprovedUnpublishedMessage();
+
+    await Promise.all(
+      unpublishedMessages.map(async (message) => {
+        try {
+          this.logger.debug(
+            `Message #${message.id}: Sending message to Discord`,
+          );
+          await this.discordService.sendMessage(ComplainMessage(message));
+
+          this.logger.debug(`Message #${message.id}: Marking as 'published'`);
+          await this.strapiService.setPublished(message.id);
+
+          this.logger.log(`Synced: #${message.id}`);
+        } catch (e) {
+          this.logger.error(`Sync failed: #${message.id}`);
+          this.logger.error(e);
+        }
+      }),
+    );
+
+    this.logger.log("End syncing approved messages.");
+  }
+}
