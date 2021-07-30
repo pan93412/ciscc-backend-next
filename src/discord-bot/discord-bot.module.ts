b27bd8c019ca1af165b5d3a0a18c179abad01edb
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-31 02:41:21 +0800

    feat(discord-bot/module): import StrapiModule

--- src/discord-bot/discord-bot.module.ts
+++ src/discord-bot/discord-bot.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { StrapiModule } from "../strapi/strapi.module";
 import { DiscordBotService } from "./discord-bot.service";
 
 @Module({
+  imports: [StrapiModule],
   providers: [DiscordBotService],
   exports: [DiscordBotService],
 })
