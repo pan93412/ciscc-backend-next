9d50caab26c01b1bff01db16725d98fa768fecd1
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-30 15:23:12 +0800

    feat(src/discord-bot/utility/consts): user-friendly

--- src/discord-bot/utility/consts.ts
+++ src/discord-bot/utility/consts.ts
@@ -1,5 +1,5 @@
 export const DISCORD_COMMAND_PREFIX = "CISCC ";
 export const TRASH_BIN_EMOJI = "🗑️";
 
-export const SERVICE_MESSAGE_PREFIX = "[CISCC-SERVICE-MESSAGE] ";
-export const SERVICE_MESSAGE_MATCHER = /^\[CISCC-SERVICE-MESSAGE]/;
+export const SERVICE_MESSAGE_PREFIX = "[CISCC 服務訊息] ";
+export const SERVICE_MESSAGE_MATCHER = /^\[CISCC 服務訊息]\s/;
